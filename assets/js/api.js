c9f27fa4dd7010711ab6d2620929264e195f03c8
onkbaonk <onkbaonk@gmail.com>
2026-05-09 12:36:20 +0700

    update onkhub v.2.1

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const REPO_PATH = "USERNAME_GITHUB_ANDA/NAMA_REPO_ANDA";
+const REPO_PATH = "onkbaonk/onkhub";
 const BRANCH = "main";
 
 let GITHUB_TOKEN = localStorage.getItem("github_token") || "";
